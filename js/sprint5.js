a952f7ebd34c2a6d3e8dfe88903b6c5245bf18a1
Elneto <ernestoster@gmail.com>
2020-01-17 10:41:43 +0100

    cambios viernes en la mañana

--- js/sprint5.js
+++ js/sprint5.js
@@ -1,8 +1,17 @@
-$(document).ready( function(){
-    
+
+
+
+
+$(document).ready(infogen);
+
+$("#refresh").click(function(){
+          infogen();
+    console.log("refresca la info");
+});   
  
     
-  var infogen = $.ajax({
+function infogen(){ 
+        $.ajax({
         url: "http://api.openweathermap.org/data/2.5/weather?id=3117732&units=metric&APPID=6967c0545bc9b2c52b54e1fd9e453d42",
         datatype : "json",
         type: "GET",
@@ -17,7 +26,7 @@ $(document).ready( function(){
             var dateString = moment.unix(dateUni).format("DD/MM/YYYY");
             var dia = moment.unix(dateUni).format('dddd');
             var dateNorm = new Date(dateUni*1000); 
-            var day = date.getDate(dateUni*1000);
+            
             
             
             /* seccion variables */
@@ -26,13 +35,13 @@ $(document).ready( function(){
             console.log(result);
         $("#name").html(ciudad);
         $("#mainTemp").html(Math.round(result.main.temp) + "Cº");
-        $("#maxMin").html(Math.round(result.main.temp_max) + "º" + "/" + Math.round(result.main.temp_min) + "º");
+        $("#maxMin").html(Math.round(result.main.temp_max) + "º" + " / " + Math.round(result.main.temp_min) + "º");
         $("#wind").html(result.wind.speed + " m/s");
         $("#cloud").html(result.clouds.all + " %");
         $("#imgcurrent").attr("src",iurl);
         $(".imgdias").attr("src",iurl);/* hacer variable con API de los 5 dias(horas en este caso)*/
-        $("#date").html(day + " Ten un buen día"); 
-        $("#day").html(dia + " Hola");
+        $("#day").html(dateString + " Hola");
+        $("#date").html(dia + " Ten un buen día"); 
             
             
             
@@ -48,54 +57,13 @@ $(document).ready( function(){
  
        
    });  
+                   
+                   
+    
+ }/*funcion*/
     
     
-  $("#refresh").click(function(){
-     location.reload();
-      console.log("refresca la info");
-   /*  $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?id=3117732&units=metric&APPID=6967c0545bc9b2c52b54e1fd9e453d42",
-        datatype : "json",
-        type: "GET",
-        success: function(result){
-            
-            
-          
-            var icono = result.weather[0].icon;
-            var iurl = "http://openweathermap.org/img/wn/" + icono + "@2x.png";
-            var ciudad = result.name;
-            var date = result.dt;  
-            var dateString = moment.unix(date).format("DD/MM/YYYY");
-            
-            
-            
-            
-            console.log(result);
-        $("#name").html(ciudad);
-        $("#mainTemp").html(Math.round(result.main.temp) + "Cº");
-        $("#wind").html(result.wind.speed + " m/s");
-        $("#imgcurrent").attr("src",iurl);
-        $(".imgdias").attr("src",iurl);
-        $("#date").html(dateString + " Datos actualizados");   
-            
-            
-            
-            
-            
-            
-        
-        },
-        error: function(){
-            console.log("ERROR: de este lado");
-        }
-       
- 
-       
-   }); */
-       
-       
-       });    
+     
     
     
     
-});
